9db41cc27871ef1f31caad8a41f02082015168ae
George Barton <mindflayer94@gmail.com>
2013-09-08 08:11:45 -0400

    Update __init__.py

---
+++ Roller/__init__.py
@@ -0,0 +1,14 @@
+"""
+ShadowrunnDie
+=============
+
+A Bot that allows Reddit Users to toss a pool of dice for the game Shadowrun. 
+This tool is designed to facillitate a fair dice rolling mechanism, 
+allowing users of the social networking system Reddit to play a game of Shadowrun 
+with the knowledge that all results are generated truly randomly 
+(or at least as randomly as can be attained reasonably through a computer system)
+
+Documentation
+-------------
+Documentation is available as docstrings
+"""
