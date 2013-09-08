5d856f727ec084b9ec5139e8dc46c992c62b0f7f
George Barton <mindflayer94@gmail.com>
2013-09-08 08:01:37 -0400

    Update debug.py

--- Roller/debug.py
+++ Roller/debug.py
@@ -1,8 +1,15 @@
-#A Tool to debug the Rolls of the dice
-#isOn determines whether or not the rolling debugger is on
-#Num refers to the result, is None if one seeks to just determine if the roller thinks its supposed to have edge
-#hasEdge, refers to whether or not the die exploded
 def debugRoller(isOn, num, hasEdge):
+    """A Tool to debug the Rolls of the dice
+    
+    Parameters
+    ----------
+    isOn: Boolean
+    determines whether or not the rolling debugger is on
+    Num: Int
+    refers to the result, is None if one seeks to just determine if the roller thinks its supposed to have edge
+    hasEdge: Boolean
+    refers to whether or not the die exploded
+    """
     msg = ''
     if num is not None:
         msg += str(num)
@@ -11,22 +18,38 @@ def debugRoller(isOn, num, hasEdge):
     if isOn:
         print msg
 
-#A tool to debug the known comments log
-#isOn determines whether or not the rolling debugger is on
-#commentId refers to the comment ID read from the text file, None if testing the set
-#already_done refers to the set
+
 def debugKnownCommentBuilder(commentId, already_done, isOn = True):
+    """A tool to debug the known comments log
+    
+    Parameters
+    ----------
+    isOn: Boolean
+    determines whether or not the rolling debugger is on
+    commentId: String
+    refers to the comment ID read from the text file, None if testing the set
+    already_done: Set
+    refers to the set
+    """
     if already_done is None and isOn:
         print commentId
     elif isOn:
         print '================================================================'
         print already_done
 
-#debug the reply builder
-#isOn determines whether or not the rolling debugger is on
-#pool refers to the size of the pool being responded to
-#has edge refers to whether or not edge was spent on the roll
+
 def debugBuildReply(isOn, pool, hasEdge):
+    """debug the reply builder
+    
+    Parameters
+    ----------
+    isOn: Boolean
+    determines whether or not the rolling debugger is on
+    pool: Int
+    refers to the size of the pool being responded to
+    has edge: Boolean
+    refers to whether or not edge was spent on the roll
+    """
     msg = ''
     if isOn:
         msg += 'I think someone wants me to roll with a dice pool of %d' % pool
@@ -34,10 +57,17 @@ def debugBuildReply(isOn, pool, hasEdge):
             msg += ' I think they have edge'
     print msg
 
-#debug the comment logger responsible for building the local logs
-#isOn determines whether or not the rolling debugger is on
-#commentId refers to the comment ID read from the text file, None if testing the set
+
 def debugCommentLogger(isOn, commentId):
+    """debug the comment logger responsible for building the local logs
+    
+    Parameters
+    ----------
+    isOn: Boolean
+    determines whether or not the rolling debugger is on
+    commentId: String
+    refers to the comment ID read from the text file, None if testing the set
+    """
     if isOn:
         print commentId
         print '\n'
